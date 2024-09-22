8167c314f2b3a8e15d497043e9e8bf95d9b559ed
Prince <75819360+Rajin-Saleh@users.noreply.github.com>
2024-09-22 12:15:19 +0600

    Add files via upload

---
+++ Chapter1/problems_1.py
@@ -0,0 +1,41 @@
+
+# printing poems
+
+# Chapter 1 Problems
+
+print("""
+Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+
+Then the traveler in the dark
+Thanks you for your tiny spark,
+How could he see where to go,
+If you did not twinkle so?
+
+In the dark blue sky you keep,
+Often through my curtains peep
+For you never shut your eye,
+Till the sun is in the sky.
+
+As your bright and tiny spark
+Lights the traveler in the dark,
+Though I know not what you are,
+Twinkle, twinkle, little star.
+""")
+
+# installing an external module and perform operation
+# pip3 install pyttsx3
+
+import pyttsx3
+machine_voice = pyttsx3.init()
+machine_voice.say("At this point i am simply human kind.")
+machine_voice.runAndWait()
+
+
