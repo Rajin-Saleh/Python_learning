7dd1f526b36ef67d650498fde05414e0f7c66b09
Prince <75819360+Rajin-Saleh@users.noreply.github.com>
2024-09-22 12:15:01 +0600

    Add files via upload

---
+++ Chapter1/chapter1.py
@@ -0,0 +1,12 @@
+# Modules, Comments & pip
+
+import pyjokes
+print("Programming jokes.....")
+joke = pyjokes.get_joke()
+print(joke)
+
+jokes2 = pyjokes.get_joke()
+print(jokes2)
+
+
+# creating random jokes for no reason.
